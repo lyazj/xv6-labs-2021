b3d65d2fd7dc511616ef0f1c514e3891e0f1747a
lyazj <seeson@pku.edu.cn>
2022-11-12 16:51:47 +0800

    Implement per-cpu free lists.

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,22 +21,41 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  int i;
+  char buf[] = "kmem-??";
+  if(NCPU <= 0)
+    panic("kinit: invalid cpu number");
+  if(NCPU > 99)
+    panic("kinit: too many cpus");
+  for(i = 0; i < NCPU; ++i)
+  {
+    buf[5] = '0' + NCPU / 10;
+    buf[6] = '0' + NCPU % 10;
+    initlock(&kmem[i].lock, buf);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
 void
 freerange(void *pa_start, void *pa_end)
 {
+  int i = 0;
+  struct run *r;
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+  {
+    r = (struct run *)p;
+    r->next = kmem[i].freelist;
+    kmem[i].freelist = r;
+    if(++i == NCPU)
+      i = 0;
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -47,6 +66,7 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  int id;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -56,10 +76,13 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+  id = cpuid();
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,12 +92,16 @@ void *
 kalloc(void)
 {
   struct run *r;
+  int id;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+  id = cpuid();
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[id].freelist = r->next;
+  release(&kmem[id].lock);
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
